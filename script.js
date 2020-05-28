e67c3bfa1825c52085ab6eb52d4275d7f185899d
fardinkhan <fardinkhan@uvic.ca>
2020-05-28 05:46:59 -0700

    added score feature

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var y = 0
 var x = 0
+var score = 1
 
 function computers_number() {
 
@@ -18,7 +19,6 @@ function users_number() {
 		document.querySelector("h3").innerHTML = y + " is an INVALID NUMBER! please enter a number between 1 and 10.";
 		document.querySelector("h4").innerHTML = "";
 	}
-
 }
 
 function result() {
@@ -28,23 +28,22 @@ function result() {
 		if (x == y) {
 			document.querySelector("h4").innerHTML = "YOU'RE RIGHT! 🤩 The computer chose " + x + " and you chose " + y + " too!";
 			document.getElementById("mark_pic").src = "right.png";
+			document.querySelector("h6").innerHTML = "FINAL SCORE: " + score++;
 		}
 
 		else if (x - y == 1 || y - x == 1) {
 			document.querySelector("h4").innerHTML = "OOOH CLOSE! 😬 The computer chose " + x + " and you chose " + y + "!";
 			document.getElementById("mark_pic").src = "wrong.png";
+			document.querySelector("h6").innerHTML = "SCORE: " + score++;
 		}
 
 		else {
 			document.querySelector("h4").innerHTML = "YOU'RE WRONG! 😰 The computer chose " + x + " and you chose " + y + "!";
 			document.getElementById("mark_pic").src = "wrong.png";
+			document.querySelector("h6").innerHTML = "SCORE: " + score++;
 		}
 
 		document.querySelector("h3").innerHTML = "";
 	}
 	
 }
-
-
-
-
